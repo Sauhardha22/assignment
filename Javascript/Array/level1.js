d04ae6f2abd24912469616687e37368b1e301662
Sauhardha <sauhardcr777@gmail.com>
2023-06-22 09:21:37 +0545

    second assignment done

--- Javascript/Array/level1.js
+++ Javascript/Array/level1.js
@@ -2,6 +2,18 @@ const allUsers = ['hari', 'shyam', 'thakur']
 
 allUsers.map((item,idx)=>{
     if (item === 'hari'){
-        console.log('hello')
+        console.log('true')
     }
 })
+
+
+const user= 'hari'
+const userArr = ['hari','shyam','hari','thakur']
+
+let count= 0
+userArr.map((item)=>{
+    if (item === 'hari'){
+      count = count + 1
+    }
+})
+console.log(count)
