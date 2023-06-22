1e430c699de30fd69f1d01506501a52ddf8025ba
Sauhardha <sauhardcr777@gmail.com>
2023-06-22 09:01:25 +0545

    assignment done

---
+++ Javascript/Array/level1.js
@@ -0,0 +1,12 @@
+//
+const name = 'hari'
+const allUsers = ['hari', 'shyam', 'thakur']
+
+console.log(typeof(name))
+
+allUsers.map = (item)=>{
+    console.log('hello')
+    if (item === 'hari'){
+        console.log('hello')
+    }
+}
